9e76af7509ed235196f45f9e37c249ee35f29154
Stefhany <128521007+stefhrodrigs@users.noreply.github.com>
2023-05-16 16:09:50 -0300

    Add files via upload

---
+++ exercício 7/acoes.js
@@ -0,0 +1,37 @@
+function acao1() {
+    let resp = window.document.getElementById('saida')
+    resp.innnerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += 'Clicou no segundo botão</p>'
+}
+
+function acao3(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4() {
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
+
+let resp = window.document.getElementById('saida')
+
+function acao1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2() {
+    resp.innerHTML += '<p>Clicou no segundo botão<p/>'
+}
+
+function acao3() {
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4() {
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
